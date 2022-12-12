640a61172a2dbe3c697c9d05c93854ef8701743d
willzer0 <pembunuh4444@gmail.com>
2022-12-12 23:45:35 +0700

    Dekstop Form Making

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
+import DesktopMenu from "./pages/DesktopMenu";
 import DesktopFormMaking from "./pages/DesktopFormMaking";
 import HomeMenu from "./components/HomeMenu";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import { faRoute } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div className="App">
       <DesktopFormMaking />
+      {/* <DesktopMenu /> */}
+      {/* <DesktopFormMaking /> */}
       {/*<HomePage />*/}
       {/* <LoginPage /> */}
     </div>
