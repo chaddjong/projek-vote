1d01483df282256837daa0fd1fb3bbcc25b02c14
chaddjong <richardicad85@gmail.com>
2022-12-29 18:16:53 +0800

    dashboard report

--- src/components/DashboardCard.jsx
+++ src/components/DashboardCard.jsx
@@ -2,11 +2,32 @@ import React from "react";
 
 export default function DashboardCard() {
   return (
-    <div className="flex flex-row justify-start bg-white w-[97.5vw] h-[100px] rounded-xl mt-[13px]">
-      <div className="flex flex-col">
-        <h1 className="text-[20px] mt-[3vh] border-b-2 border-black pb-[7.32px] ml-[15px] font-[400]">Untitled Question</h1>
-        <div>option 1</div>
+    <body>
+      {/* mobile layout */}
+      <div className="lg:hidden flex flex-row justify-start bg-white w-[97vw] h-max rounded-xl mt-[13px] mb-[10px]">
+        <div className="flex flex-col">
+          <h1 className="text-[3vh] mt-[4vh] border-b-2 border-black pb-[4px] ml-[15px] font-[400] w-[80vw]">
+            Untitled Question
+          </h1>
+          <div className="flex flex-col ml-[20px] mt-[30px] text-[#898989] pb-[25px]">
+            <p>option 1</p>
+            <div className="flex flex-row items-center mb-[10px]">
+              <div className="bg-[#D9D9D9] w-[235px] h-[20px] ml-[5vw]"></div>
+              <p className="ml-[9px]">xxx</p>
+            </div>
+            <p>option 2</p>
+            <div className="flex flex-row items-center mb-[10px]">
+              <div className="bg-[#D9D9D9] w-[235px] h-[20px] ml-[5vw]"></div>
+              <p className="ml-[9px]">xxx</p>
+            </div>
+            <p>option 3</p>
+            <div className="flex flex-row items-center mb-[10px]">
+              <div className="bg-[#D9D9D9] w-[235px] h-[20px] ml-[5vw]"></div>
+              <p className="ml-[9px]">xxx</p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </body>
   );
 }
--- src/pages/ResponseWarning.jsx
+++ src/pages/ResponseWarning.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 export default function warning() {
   return (
-    <body className="bg-white flex flex-col justify-center items-center h-[100vh] w-[100%]">
-      <div className="text-center text-[5vw] lg:text-[3vw] font-bold">
-        <p>YOU HAD ALREADY</p>
-        <p>FILLED THIS FORM.</p>
-        <p>YOU CAN ONLY FILL</p>
-        <p>THIS FORM ONCE</p>
-        <br />
-        <Link to="/home">
-          <button className=" underline">go back to home page</button>
-        </Link>
+    <body>
+      <div className="bg-white flex flex-col justify-center items-center h-[100vh] w-[100%]">
+        <div className="text-center text-[5vw] lg:text-[3vw] font-bold">
+          <p>YOU HAD ALREADY</p>
+          <p>FILLED THIS FORM.</p>
+          <p>YOU CAN ONLY FILL</p>
+          <p>THIS FORM ONCE</p>
+          <br />
+          <Link to="/home">
+            <button className=" underline">go back to home page</button>
+          </Link>
+        </div>
       </div>
     </body>
   );
