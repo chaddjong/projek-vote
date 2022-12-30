e3f7bb611e9b993a95866d7555c6accb42430e46
willzer0 <pembunuh4444@gmail.com>
2022-12-30 08:56:38 +0700

    memperbaiki layout publicform"

--- src/components/PublicFormThumbnail.jsx
+++ src/components/PublicFormThumbnail.jsx
@@ -7,7 +7,7 @@ export default function PublicFormThumbnail(props) {
       id="btn_PbcForm"
       className="flex justify-evenly bg-white w-[41%] h-[13vh] lg:h-[19vh] rounded-2xl hover:bg-[#FFB546] mx-[4.2%] mt-5 lg:bg-[#d9d9d9] active:bg-orange-400"
     >
-      <button className="flex flex-col justify-start items-start w-[100%] h-[100%]">
+      <button className="flex flex-col w-[100%] h-[100%]">
         <p className="self-end xl:text-[14px]">Expired Date</p>
         <p className="self-end xl:text-[14px]">xx.xx</p>
         <h3 className="text-[18px]">{title}</h3>
