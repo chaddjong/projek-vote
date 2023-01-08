d2d88e3a0287cee6b9bdc514c16d1a9e6377b451
chaddjong <richardicad85@gmail.com>
2023-01-08 23:08:26 +0800

    page voting

--- src/components/VotingCard.jsx
+++ src/components/VotingCard.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 import OptionVote from "../components/OptionVote";
+
 export default function Card() {
   return (
-    <div className="w-[29%] max-[1023px]:w-[95%] max-[1023px]:mt-4 h-auto pb-2 bg-white rounded-xl flex flex-col">
-      <p className="mt-2 font-inter text-[#000000] ml-[5%] w-[90%]">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-        recusandae deserunt assumenda iusto animi est officia. Excepturi
-        accusamus fugit tempora! Eius est, sequi libero voluptatem odio
-        blanditiis temporibus pariatur accusamus!
-      </p>
-      <hr className="bg-black h-[2px] w-[90%] flex place-self-center shadow-sm shadow-black" />
-      <OptionVote />
-    </div>
+    <body>
+      <div className="w-[29%] max-[1023px]:w-[95%] max-[1023px]:mt-4 h-auto pb-2 bg-white rounded-xl flex flex-col max-[1023px]:hidden">
+        <p className="mt-2 font-inter text-[#000000] ml-[5%] w-[90%]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+          recusandae deserunt assumenda iusto animi est officia. Excepturi
+          accusamus fugit tempora! Eius est, sequi libero voluptatem odio
+          blanditiis temporibus pariatur accusamus!
+        </p>
+        <hr className="bg-black h-[2px] w-[90%] flex place-self-center shadow-sm shadow-black" />
+        <OptionVote />
+      </div>
+
+      {/* mobile layout */}
+      <div className="lg:hidden w-[97vw] bg-white h-auto mt-[10px] rounded-xl flex flex-col justify-center p-5">
+        <p className="border-black border-b-[2px] pb-[2px]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+          convallis ligula diam, sit amet hendrerit libero sodales quis.
+        </p>
+        <div className="mt-[2.5vh]">
+          <OptionVote />
+          <OptionVote />
+        </div>
+      </div>
+    </body>
   );
 }
--- src/pages/Voting.jsx
+++ src/pages/Voting.jsx
@@ -19,36 +19,57 @@ export default function Voting() {
   }, []);
 
   return (
-    <body className="bg-[#D9D9D9] h-full pb-4">
-      <div className="flex flex-col justify-center items-center ">
-        <div className=" bg-white w-full lg:w-[30%] h-auto rounded-b-2xl lg:mb-5 lg:h-auto lg:pb-4 flex flex-col">
-          <div>
-            {/* <Link to="/home"> */}
-            <button class="h-[4vh] w-[8%] sm:w-[5%] lg:w-[2vw] m-4">
-              <img src={BackArrow} class="h-[4vh] w-[100%]" />
+    <body className="">
+      <div className="bg-[#D9D9D9] h-full pb-4  max-[1023px]:hidden">
+        <div className="flex flex-col justify-center items-center">
+          <div className=" bg-white w-full lg:w-[30%] h-auto rounded-b-2xl lg:mb-5 lg:h-auto lg:pb-4 flex flex-col">
+            <div>
+              {/* <Link to="/home"> */}
+              <button class="h-[4vh] w-[8%] sm:w-[5%] lg:w-[2vw] m-4">
+                <img src={BackArrow} class="h-[4vh] w-[100%]" />
+              </button>
+              {/* </Link> */}
+            </div>
+            <div className="mt-5 ml-5 lg:mt-8">
+              <p className=" text-black font-normal w-[95%] text-4xl focus:outline-none lg:text-[3vw]">
+                {/* {data.title} */}
+              </p>
+              <br></br>
+              <p className="text-black font-normal opacity-50 text-base leading-6 focus:outline-none ml-1 w-[95%] lg:text-[1.5vw]">
+                {/* {data.information.description} */}
+              </p>
+            </div>
+          </div>
+          <VotingCard />
+          <div className="w-[29%] h-auto">
+            <button className="bg-[#656565] py-2 px-8 shadow-sm shadow-black rounded-lg mt-2">
+              Submit
             </button>
-            {/* </Link> */}
           </div>
-          <div className="mt-5 ml-5 lg:mt-8">
-            <p className=" text-black font-normal w-[95%] text-4xl focus:outline-none lg:text-[3vw]">
-              {data.title}
-            </p>
-            <br></br>
-            <p className="text-black font-normal opacity-50 text-base leading-6 focus:outline-none ml-1 w-[95%] lg:text-[1.5vw]">
-              {data.information.description}
-            </p>
-          </div>
-        </div>
-        <VotingCard />
-        <div className="w-[29%] h-auto">
-          <button className="bg-[#656565] py-2 px-8 shadow-sm shadow-black rounded-lg mt-2">
-            Submit
-          </button>
         </div>
       </div>
 
       {/* mobile layout */}
-      <div className="lg:hidden">test</div>
+      <div className="flex flex-col items-center min-h-screen bg-[#94BBFF] lg:hidden w-full">
+        <div className="bg-white w-full h-max pb-[10px] rounded-b-xl">
+          {/* <Link to="/home"> */}
+          <button className="mt-[25px] ml-[20px] mb-[10px]">
+            <img src={BackArrow} className="w-[7.5vw]" />
+          </button>
+          {/* </Link> */}
+          <h1 className="text-[10vw] ml-[22px]">Untitled form</h1>
+          <p className="ml-[25px] w-[92.5vw] opacity-50">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+            convallis ligula diam, sit amet hendrerit libero sodales quis.
+          </p>
+        </div>
+        <VotingCard />
+        <div className="flex justify-start w-[90vw] mt-[26px]">
+          <button className="text-white bg-[#3E8BFF] w-[120px] h-[50px] rounded-2xl font-extrabold">
+            SUBMIT
+          </button>
+        </div>
+      </div>
     </body>
   );
 }
